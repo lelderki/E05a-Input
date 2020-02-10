8dcc6938d9f66bc0ab61b59b4b97ee7534fecc08
Lawren E <59849750+lelderki@users.noreply.github.com>
2020-02-10 16:56:38 -0500

    Update Label.gd

--- Scripts/Label.gd
+++ Scripts/Label.gd
@@ -1,8 +1,9 @@
 extends Label
 
-
 func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	var mouse_pos = get_viewport().get_mouse_position()
+	print(mouse_pos.x)
+	text = str(mouse_pos.x) + ", " + str(mouse_pos.y)
